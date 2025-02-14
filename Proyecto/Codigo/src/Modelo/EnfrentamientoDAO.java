7df19b822d4ca49f56d7f93529d6b35e947fb6de
“Unax” <unax.iriondo@ikasle.egibide.org>
2025-02-14 13:50:30 +0100

    EnfrentamientoController terminado

--- Proyecto/Codigo/src/Modelo/EnfrentamientoDAO.java
+++ Proyecto/Codigo/src/Modelo/EnfrentamientoDAO.java
@@ -1,4 +1,13 @@
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnfrentamientoDAO {
+    static List<Enfrentamiento> enfrentamientos=new ArrayList<>();
+
+    public static void agregarEnfrentamientos (Enfrentamiento enfrentamiento){
+        enfrentamientos.add(enfrentamiento);
+    }
+
 }
